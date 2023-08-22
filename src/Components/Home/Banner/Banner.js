a047a4453438e5c0b84c569a1b8c506e86c38608
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-08-22 22:08:28 +0600

    installed react icon added sidebar and carousel

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -3,9 +3,13 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <h1>This is banner</h1>
-
-
+        <div className="dropdown dropdown-hover">
+  <label tabIndex={0} className="btn m-1">Hover</label>
+  <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
             
         </div>
     );
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,18 +1,277 @@
-import React from 'react';
+import { GiIceSkate, GiTravelDress } from "react-icons/gi";
+import {
+  FaAlignLeft,
+  FaCircleDollarToSlot,
+  FaPersonBooth,
+  FaPersonDressBurst,
+  FaRegStar,
+  FaShirt,
+} from "react-icons/fa6";
+import { PiDotsThreeOutlineThin } from "react-icons/pi";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is hom</h2>
-            <button className="btn btn-active">Default</button>
-<button className="btn btn-active btn-neutral">Neutral</button>
-<button className="btn btn-active btn-primary">Primary</button>
-<button className="btn btn-active btn-secondary">Secondary</button>
-<button className="btn btn-active btn-accent">Accent</button>
-<button className="btn btn-active btn-ghost">Ghost</button>
-<button className="btn btn-active btn-link">Link</button>
+  return (
+    <div className="container mx-auto w-9/12 h-[35rem] flex">
+      <div className="w-4/12 border shadow-md">
+        <div className="dropdown  dropdown-right  w-full border ">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 bg-black text-white hover:text-sky-400">
+              <FaAlignLeft className="mt-2 text-xl ml-3"></FaAlignLeft> <h3 className=" text-xl  ">Browse Our Sales</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
         </div>
-    );
+
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400 ">
+              <FaRegStar className="mt-2 text-xl ml-3"></FaRegStar> <h3 className=" text-xl ">Newest Items</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <FaCircleDollarToSlot className="mt-2 text-xl ml-3"></FaCircleDollarToSlot>{" "}
+              <h3 className=" text-xl ">ON Sale</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <FaPersonDressBurst className="mt-2 text-xl ml-3"></FaPersonDressBurst>{" "}
+              <h3 className=" text-xl ">Accessories</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-full">
+            <div className="grid grid-cols-4 gap-4">
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <FaPersonBooth className="mt-2 text-xl ml-3"></FaPersonBooth> <h3 className=" text-xl ">Backpack</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <GiTravelDress className="mt-2 text-xl ml-3"></GiTravelDress> <h3 className=" text-xl ">Dresses</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <GiIceSkate className="mt-2 text-xl ml-3"></GiIceSkate> <h3 className=" text-xl ">Shoes</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <FaShirt className="mt-2 text-xl ml-3"></FaShirt> <h3 className=" text-xl ">Tops & T-Shirts</h3>
+            </div>
+          </label>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
+        <div className="dropdown dropdown-hover dropdown-right w-full  border">
+          <label tabIndex={0}>
+            <div className="flex gap-2 p-4 hover:text-sky-400">
+              <PiDotsThreeOutlineThin className="mt-2 text-xl ml-3"></PiDotsThreeOutlineThin>{" "}
+              <h3 className=" text-xl ">Others</h3>
+            </div>
+          </label>
+          {/* <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100  w-52">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul> */}
+        </div>
+      </div>
+
+      <div className="w-full ">
+        <div className="flex justify-center">
+          <div className="carousel w-11/12  h-[35rem]">
+            <div id="slide1" className="carousel-item relative w-full">
+              <div className="absolute p-11 m-11">
+                <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
+                <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
+                <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
+                <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
+                  {" "}
+                  Shop Now{" "}
+                </a>
+              </div>
+              <img
+                src="https://www.searchenginejournal.com/wp-content/uploads/2022/08/google-shopping-ads-6304dccb7a49e-sej-760x400.webp"
+                className="w-full"
+              />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+              <div className="absolute p-11 m-11">
+                <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
+                <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
+                <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
+                <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
+                  {" "}
+                  Shop Now{" "}
+                </a>
+              </div>
+              <img
+                src="https://i0.wp.com/stanzaliving.wpcomstaging.com/wp-content/uploads/2022/05/Malls-in-Mumbai.jpg?fit=1000%2C678&ssl=1"
+                className="w-full"
+              />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide3" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+              <div className="absolute p-11 m-11">
+                <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
+                <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
+                <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
+                <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
+                  {" "}
+                  Shop Now{" "}
+                </a>
+              </div>
+              <img
+                src="https://image-tc.galaxy.tf/wijpeg-5hwpt4qbfrteko9t5pkqcyqn3/shopping-resize_standard.jpg?crop=69%2C0%2C1783%2C1337"
+                className="w-full"
+              />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide2" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide4" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+              <div className="absolute p-11 m-11">
+                <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
+                <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
+                <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
+                <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
+                  {" "}
+                  Shop Now{" "}
+                </a>
+              </div>
+              <img
+                src="https://st4.depositphotos.com/4678277/40811/i/450/depositphotos_408110334-stock-photo-full-length-body-size-view.jpg"
+                className="w-full"
+              />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide3" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide1" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
