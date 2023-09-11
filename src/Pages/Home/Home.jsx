4cbd05e69e3c761c03010fe70dff82cddf1b1377
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-09-11 23:47:23 +0600

    ..

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import FeaturedItems from './FeaturedItems/FeaturedItems';
 import NewArrivals from './NewArrivals/NewArrivals';
-<<<<<<< HEAD
-import GetInTouch from '../Shared/GetInTouch/GetInTouch';
+
 import Trendings from './Trendings/Trendings';
 import AdSection from './AdSection/AdSection';
-=======
-import LatestNews from './LatestNews/LatestNews';
-import GetInTouch from '../Shared/GetInTouch/GetInTouch';
->>>>>>> 89536c37bfdd7ea6e58b9a1090e8c28738a25828
+
 
 
 const Home = () => {
@@ -21,17 +17,7 @@ const Home = () => {
             <AdSection></AdSection>
             
             <FeaturedItems />
-<<<<<<< HEAD
-            <GetInTouch></GetInTouch>
-
-
-=======
-            <LatestNews></LatestNews>
-            <GetInTouch></GetInTouch>
->>>>>>> 89536c37bfdd7ea6e58b9a1090e8c28738a25828
-        </>
-
-
+</>
     );
 };
 
