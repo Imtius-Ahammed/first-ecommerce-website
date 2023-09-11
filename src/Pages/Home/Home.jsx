d13d34ea21a104f0c04964395147049b8dff55b1
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-09-11 23:31:09 +0600

    added Trending section and AsSection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import FeaturedItems from './FeaturedItems/FeaturedItems';
 import NewArrivals from './NewArrivals/NewArrivals';
+import GetInTouch from '../Shared/GetInTouch/GetInTouch';
+import Trendings from './Trendings/Trendings';
+import AdSection from './AdSection/AdSection';
 
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
+            <Trendings></Trendings>
             <NewArrivals />
+            <AdSection></AdSection>
+            
             <FeaturedItems />
+            <GetInTouch></GetInTouch>
 
 
         </>
---
+++ src/Pages/Home/Trendings/Trendings.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Trendings = () => {
+    return (
+        <div className='max-w-6xl mx-auto  '>
+            <div className='text-center lg:m-0 mx-4'>
+                <h1 className='text-4xl font-bold '>Trendings Categories</h1>
+                <p>Find a bright ideal to suit your taste with our great selection of suspension.</p>
+            </div>
+
+            <div className='grid lg:grid-cols-5 md:grid-cols-5 grid-cols-3 gap-4 my-12 place-items-center lg:mx-0 mx-4'>
+                <div >
+                    <img className=' rounded-full' src="/src/Assets/Img/category-1.jpg" alt="" />
+                    <div className='text-center p-5'>
+                <h1 className='lg:text-2xl md:text-2xl font-medium hover:underline hover:text-red-500 '>Tops & T-Shirt</h1>
+                <p>(5)Products</p>
+            </div>
+
+                </div>
+                <div >
+                    <img className=' rounded-full' src="/src/Assets/Img/category-2.jpg" alt="" />
+                    <div className='text-center p-5'>
+                <h1 className='lg:text-2xl md:text-2xl font-medium hover:underline hover:text-red-500 '>Dresses</h1>
+                <p>(4)Products</p>
+            </div>
+
+                </div>
+                <div >
+                    <img className=' rounded-full' src="/src/Assets/Img/category-3.jpg" alt="" />
+                    <div className='text-center p-5'>
+                <h1 className='lg:text-2xl md:text-2xl font-medium hover:underline hover:text-red-500 '>Shoes</h1>
+                <p>(8)Products</p>
+            </div>
+
+                </div>
+                <div >
+                    <img className=' rounded-full' src="/src/Assets/Img/category-4.jpg" alt="" />
+                    <div className='text-center p-5'>
+                <h1 className='lg:text-2xl md:text-2xl font-medium hover:underline hover:text-red-500 '>Backpack</h1>
+                <p>(3)Products</p>
+            </div>
+
+                </div>
+                <div >
+                    <img className=' rounded-full' src="/src/Assets/Img/category-5.jpg" alt="" />
+                    <div className='text-center p-5'>
+                <h1 className='lg:text-2xl md:text-2xl font-medium hover:underline hover:text-red-500 '>Accessories</h1>
+                <p>(7)Products</p>
+            </div>
+
+                </div>
+                
+            
+
+            </div>
+        </div>
+    );
+};
+
+export default Trendings;
