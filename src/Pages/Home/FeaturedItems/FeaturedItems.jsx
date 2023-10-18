f2813a765a58967e35bb43798ad51bfe63393352
MD Rahi <fhrstation21@gmail.com>
2023-10-18 13:01:45 +0600

    add men and children filtered products in featured items

--- src/Pages/Home/FeaturedItems/FeaturedItems.jsx
+++ src/Pages/Home/FeaturedItems/FeaturedItems.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from '../../../Components/ProductCard/ProductCard';
 
 const FeaturedItems = ({ option }) => {
 
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [filtered, setFiltered] = useState(false);
+
+    const handleFilter = (category) => {
+        const items = option.filter(product => product.category === category)
+        console.log(category);
+        setFilteredProducts(items);
+        setFiltered(true)
+    }
+
+    console.log(filteredProducts);
+
     return (
         <div className="max-w-6xl mx-auto my-20 relative">
             <h1 className='text-center text-4xl font-semibold my-6'>Featured Items</h1>
+            <div className='flex h-12 items-center justify-center gap-10'>
+                <button
+                    onClick={() => handleFilter("Men's")}
+                    className='text-xl font-medium hover:text-orange-600 hover:border-b-2 border-orange-600'>
+                    For Men
+                </button>
+                <button
+                    onClick={() => handleFilter("Children's")}
+                    className='text-xl font-medium hover:text-orange-600 hover:border-b-2 border-orange-600'>
+                    For Childrens
+                </button>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 py-8 px-12 sm:px-0 cursor-pointer'>
                 {
-                    option.map(products => {
-                        return <ProductCard key={products._id} option={products} />
-                    })
+                    !filtered ?
+                        <>
+                            {
+                                option.slice(0,8).map(products => {
+                                    return <ProductCard key={products._id} option={products} />
+                                })
+                            }
+                        </>
+                        :
+                        <>
+                            {
+                                filteredProducts.slice(0,8).map(products => {
+                                    return <ProductCard key={products._id} option={products} />
+                                })
+                            }
+                        </>
                 }
             </div>
         </div>
--- src/Pages/Home/NewArrivals/NewArrivals.jsx
+++ src/Pages/Home/NewArrivals/NewArrivals.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from '../../../Components/ProductCard/ProductCard';
 
-const NewArrivals = ({option}) => {
+const NewArrivals = ({ option }) => {
+
+    const [load, setLoad] = useState(false)
+    const [hideLoad, setHideLoad] = useState(false)
+
+    const handleLoad = () => {
+        setLoad(true);
+        setHideLoad(true)
+    }
 
     return (
         <div className='max-w-6xl mx-auto my-20'>
@@ -9,13 +17,28 @@ const NewArrivals = ({option}) => {
             <p className='text-center text-lg text-gray-500'>Add our new arrivals to your weekly lineup</p>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 py-8 px-12 sm:px-0 cursor-pointer'>
                 {
-                    option.map(products => {
-                        return <ProductCard key={products._id} option={products} />
-                    })
+                    !load ?
+                        <>
+                            {
+                                option.slice(0, 8).map(products => {
+                                    return <ProductCard key={products._id} option={products} />
+                                })
+                            }
+                        </>
+                        :
+                        <>
+                            {
+                                option.map(products => {
+                                    return <ProductCard key={products._id} option={products} />
+                                })
+                            }
+                        </>
                 }
             </div>
             <div className='text-center'>
-                <button className=' bg-stone-800 text-lg font-medium text-white rounded-none border-0 normal-case px-10 py-4 hover:bg-emerald-400'>Load More</button>
+                <button onClick={handleLoad} className={!hideLoad ? 'bg-stone-800 text-lg font-medium text-white rounded-none border-0 normal-case px-10 py-4 hover:bg-emerald-400' : 'hidden'}>
+                    Load More
+                    </button>
             </div>
         </div>
     );
