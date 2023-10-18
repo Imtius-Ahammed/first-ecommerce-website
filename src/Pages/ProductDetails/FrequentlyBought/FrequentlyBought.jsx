b08e9dcb3d395b3acc7b8217cac46a7c4a08323d
Imtius-Ahammed <ahammedimtius098@gmail.com>
2023-10-18 21:31:30 +0600

    add similar product api and new features on product details page

--- src/Pages/ProductDetails/FrequentlyBought/FrequentlyBought.jsx
+++ src/Pages/ProductDetails/FrequentlyBought/FrequentlyBought.jsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const FrequentlyBought = ({ productDetails }) => {
+const FrequentlyBought = ({ productDetails, relatedProducts }) => {
+  const { image, name, price, _id, category } = productDetails;
+  const [relatedProduct, setRelatedProduct] = useState({});
+  
+  useEffect(() => {
+    try {
+      fetch(`http://localhost:5000/similarProduct/${price}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setRelatedProduct(data);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
 
-    const { image, name, price, _id } = productDetails;
 
-    return (
-        <div>
-            <h1 className='lg:text-3xl font-medium border-none mt-8 mb-2 lg:px-0 px-3'>Frequently Bought Together</h1>
-            <hr />
+  return (
+    <div>
+      <h1 className="lg:text-3xl font-medium border-none mt-8 mb-2 lg:px-0 px-3">Frequently Bought Together</h1>
+      <hr />
 
-            <div className='flex items-center gap-4 my-8 border-none lg:px-0 px-3'>
-                <div className='flex gap-2 '>
-                    <div><img className='w-28' src={image} /></div>
-                    <div className='flex items-center'>+</div>
-                    <div><img className='w-28' src="/src/Assets/Img/product-10-100x100.jpg" alt="" /></div>
-                </div>
-
-                <div className='flex flex-col'>
-                    <h1>Price for all: <span className='text-red-500'>${price}</span></h1>
-                    <button className='px-4 py-1 bg-orange-400 rounded-lg mt-3 font-semibold hover:bg-orange-600 text-white' type="submit">Add All to Cart</button>
-                </div>
-            </div>
-            <div className='border-none'>
-                <ul className='list-disc lg:ml-4 ml-6'>
-                    <li>This Product: {name} – ${price}</li>
-                    <li>Checked Wool Blend Mini Skirt – $29.00</li>
-                </ul>
-            </div>
+      <div className="flex items-center gap-4 my-8 border-none lg:px-0 px-3">
+        <div className="flex items-center justify-center gap-2 ">
+          <div>
+            <img className="w-28 h-36" src={image} />
+          </div>
+          <div className="flex items-center">+</div>
+          <div>
+            <img className="w-28 h-36" src={relatedProduct.image} alt="" />
+          </div>
         </div>
-    );
+
+        <div className="flex flex-col">
+          <h1>
+            Price for all: <span className="text-red-500">${parseInt(price) + parseInt(relatedProduct.price)}</span>
+          </h1>
+          <button
+            className="px-4 py-1 bg-orange-400 rounded-lg mt-3 font-semibold hover:bg-orange-600 text-white"
+            type="submit"
+          >
+            Add All to Cart
+          </button>
+        </div>
+      </div>
+      <div className="border-none">
+        <ul className="list-disc lg:ml-4 ml-6">
+          <li>
+            This Product: {name} – ${price}
+          </li>
+          <li>{relatedProduct.name} – ${relatedProduct.price}</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default FrequentlyBought;
--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -5,16 +5,20 @@ import Social from './Social/Social';
 import FrequentlyBought from './FrequentlyBought/FrequentlyBought';
 import DetailsBtn from './DetailsBtn/DetailsBtn';
 import Details from './Details/Details';
+import useProducts from '../../Hooks/useProducts';
 
 
 const ProductDetails = () => {
-
     const productDetails = useLoaderData();
-
     const [btn, setHandleBtn] = useState('Descriptions');
     const [showImage, setShowImage] = useState(null);
     const [hideImage, setHideImage] = useState(false);
+    
+    const {products} = useProducts();
+    const relatedProducts=(category) => products.filter(product=>{
+        product.category=== category;
 
+    })
     const handleBtn = (componentName) => {
         setHandleBtn(componentName);
     };
@@ -31,7 +35,7 @@ const ProductDetails = () => {
                 showImage={showImage}
                 hideImage={hideImage}
                 productDetails={productDetails} />
-            <FrequentlyBought productDetails={productDetails} />
+            <FrequentlyBought relatedProducts={relatedProducts} productDetails={productDetails} />
             <DetailsBtn
                 btn={btn}
                 handleBtn={handleBtn} />
