e0c2402c1ce2a4f0aa8c03cb7d5113d0f37bdec7
MD Rahi <fhrstation21@gmail.com>
2023-12-06 00:25:04 +0600

    banner re design

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -5,6 +5,38 @@ import {
 
 import React from "react";
 import ProductCard from "../../../Components/ProductCard/ProductCard";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { Link } from "react-router-dom";
+
+const bannerData = [
+  {
+    h1: 'Winter-23',
+    h2: 'YEAR-2023',
+    h3: 'Top Trend',
+    img: 'http://surl.li/nyiev'
+  },
+  {
+    h1: 'Winter-23',
+    h2: 'YEAR-2023',
+    h3: 'Top Trend',
+    img: 'http://surl.li/nyihm'
+  },
+  {
+    h1: 'Winter-23',
+    h2: 'YEAR-2023',
+    h3: 'Top Trend',
+    img: 'http://surl.li/nyiij'
+  },
+  {
+    h1: 'Winter-23',
+    h2: 'YEAR-2023',
+    h3: 'Top Trend',
+    img: 'http://surl.li/nyiiu'
+  },
+]
 
 const Banner = ({ categories, selectedProducts, selected, handleComponent }) => {
 
@@ -41,103 +73,30 @@ const Banner = ({ categories, selectedProducts, selected, handleComponent }) => 
 
       {
         !selected ?
-          <div div className="w-full mx-4">
-            <div className="flex justify-center">
-              <div className="carousel w-11/12  h-[35rem]">
-                <div id="slide1" className="carousel-item relative w-full">
-                  <div className="absolute p-11 m-11">
-                    <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
-                    <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
-                    <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
-                    <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
-                      {" "}
-                      Shop Now{" "}
-                    </a>
-                  </div>
-                  <img
-                    src="https://www.searchenginejournal.com/wp-content/uploads/2022/08/google-shopping-ads-6304dccb7a49e-sej-760x400.webp"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide2" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                  <div className="absolute p-11 m-11">
-                    <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
-                    <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
-                    <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
-                    <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
-                      {" "}
-                      Shop Now{" "}
-                    </a>
-                  </div>
-                  <img
-                    src="https://i0.wp.com/stanzaliving.wpcomstaging.com/wp-content/uploads/2022/05/Malls-in-Mumbai.jpg?fit=1000%2C678&ssl=1"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide3" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                  <div className="absolute p-11 m-11">
-                    <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
-                    <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
-                    <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
-                    <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
-                      {" "}
-                      Shop Now{" "}
-                    </a>
-                  </div>
-                  <img
-                    src="https://image-tc.galaxy.tf/wijpeg-5hwpt4qbfrteko9t5pkqcyqn3/shopping-resize_standard.jpg?crop=69%2C0%2C1783%2C1337"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide4" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                  <div className="absolute p-11 m-11">
-                    <h3 className="text-xl bg-red-500 text-white w-28 p-2">Winter-23</h3>
-                    <h1 className="text-5xl font-bold mt-5"> SS-2023</h1>
-                    <h1 className="text-5xl font-bold mb-5"> Top Trend</h1>
-                    <a className="btn btn-primary mt-6 btn-xl hover:bg-red-600 " href="#" role="button">
-                      {" "}
-                      Shop Now{" "}
-                    </a>
-                  </div>
-                  <img
-                    src="https://st4.depositphotos.com/4678277/40811/i/450/depositphotos_408110334-stock-photo-full-length-body-size-view.jpg"
-                    className="w-full"
-                  />
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">
-                      ❮
-                    </a>
-                    <a href="#slide1" className="btn btn-circle">
-                      ❯
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="w-full lg:w-8/12">
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+              {
+                bannerData.map((data, index) => {
+                  const { h1, h2, h3, img } = data;
+                  return <SwiperSlide key={index}>
+                    <div className="relative w-full">
+                      <div className="absolute flex items-center justify-center flex-col gap-5 opacity-70 w-full h-full bg-[#f5f3f3] p-12 lg:p-20">
+                        <h1 className="text-lg text-center bg-red-500 text-white p-2 left-0 top-0 absolute">{h1}</h1>
+                        <h2 className="text-2xl text-center font-bold">{h2}</h2>
+                        <h3 className="text-4xl text-center font-bold">{h3}</h3>
+                        <Link to='/shops' className="px-3 py-1 mt-6 uppercase bg-black text-white font-bold w-full lg:w-1/4 mx-auto text-center hover:bg-red-600 " role="button">
+                          Shop Now
+                        </Link>
+                      </div>
+                      <img
+                        src={img}
+                        className="w-full h-96"
+                      />
+                    </div>
+                  </SwiperSlide>
+                })
+              }
+            </Swiper>
           </div>
           :
           <div className="w-full mx-4">
